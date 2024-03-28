098a38013adb45dabcfdcca8fd3d1a94b11c977b
hessikaveh <hesam.kaveh@gmail.com>
2024-03-28 17:57:57 +0100

    fix github action

--- next.config.js
+++ next.config.js
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
