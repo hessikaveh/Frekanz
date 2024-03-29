ab92be432256556c6fe8488b987ce83e473978e0
hkaveh <hessamoddin.kaveh@cern.ch>
2024-03-29 14:53:37 +0100

    Yet another way of introducing the env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    SITE_URL:
+      process.env.NEXT_PUBLIC_SITE_URL || process.env.NEXT_PUBLIC_VERCEL_URL,
+  },
+};
 
 module.exports = nextConfig;
