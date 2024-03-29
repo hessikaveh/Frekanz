8cf539ae4149095c2c121558159274e25d9582d0
hessikaveh <hesam.kaveh@gmail.com>
2024-03-29 23:06:34 +0100

    adding simple authentication

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ function getBaseUrl() {
 }
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/********",
+      },
+    ],
+  },
   env: {
     SITE_URL: getBaseUrl(),
   },
