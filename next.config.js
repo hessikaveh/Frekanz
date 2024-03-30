a1431698a824806843a41f36b9d1a86407950347
hkaveh <hessamoddin.kaveh@cern.ch>
2024-03-30 20:29:42 +0100

    add google as a auth provider

--- next.config.js
+++ next.config.js
@@ -15,6 +15,12 @@ const nextConfig = {
         port: "",
         pathname: "/u/********",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/a/********",
+      },
     ],
   },
   env: {
