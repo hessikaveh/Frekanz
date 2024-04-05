f9d433c24a7360bbed57d20b78bdc1d5e414e2d0
hkaveh <hessamoddin.kaveh@cern.ch>
2024-04-05 17:13:38 +0200

    hide some paths from unathurised user

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,8 +1,7 @@
+import AuthCheck from "@/app/components/CustomComponents/AuthCheck";
 import { prisma } from "@/lib/prisma";
 import { Metadata } from "next";
-import { getServerSession } from "next-auth";
 import Image from "next/image";
-import { redirect } from "next/navigation";
 
 interface Props {
   params: {
@@ -18,24 +17,22 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 export default async function UserProfile({ params }: Props) {
   const user = await prisma.user.findUnique({ where: { id: params.id } });
   const { name, bio, image, id } = user ?? {};
-  const session = await getServerSession();
 
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
   return (
-    <div>
-      <h1>{name}</h1>
+    <AuthCheck>
+      <div>
+        <h1>{name}</h1>
 
-      <Image
-        src={image ?? "/peeps.svg"}
-        alt={`${name}'s profile`}
-        width={128}
-        height={128}
-      />
+        <Image
+          src={image ?? "/peeps.svg"}
+          alt={`${name}'s profile`}
+          width={128}
+          height={128}
+        />
 
-      <h3>Bio</h3>
-      <p>{bio}</p>
-    </div>
+        <h3>Bio</h3>
+        <p>{bio}</p>
+      </div>
+    </AuthCheck>
   );
 }
