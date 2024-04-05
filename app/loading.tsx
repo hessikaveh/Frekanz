786b3f3f7cafe95e3df1eb29802515be8e04dbac
hessikaveh <hesam.kaveh@gmail.com>
2024-04-05 20:26:04 +0200

    add a loading page

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default async function Loading() {
+  return (
+    <div className="flex m-8 items-center justify-center align-middle">
+      <span className="loading loading-infinity loading-lg"></span>
+    </div>
+  );
+}
